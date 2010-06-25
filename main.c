0d2186cc22556df1b955382123d4c00461b8717e
Alessandro Rubini <rubini@gnudd.com>
2010-06-25 23:00:51 +0200

    move work to bubl_work, with a new stack pointer
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- main.c
+++ main.c
@@ -20,6 +20,9 @@ static unsigned ram_test(int base)
 	return size << 2;
 }
 
+static void bubl_work(void);
+
+/* This functions turns on the system, and calls the working function */
 void bubl_main(void)
 {
 	unsigned ramsize;
@@ -39,7 +42,15 @@ void bubl_main(void)
 	printk("RAM: 0x%08x bytes (%i KiB, %i MiB)\n",
 	       ramsize, ramsize >> 10, ramsize >> 20);
 
-	/* Nothing more to do by now */
+	/* Now move the stack pointer to RAM */
+	asm("sub sp, %0, #16" : : "r" (RAMADDR + ramsize) : "memory");
+	bubl_work();
+}
+
+/* This is the real work being done: the stack pointer is not at end-of-ram */
+void __attribute__((noreturn, noinline)) bubl_work(void)
+{
+	/* Nothing to do by now */
 	while (1)
 		;
 }
