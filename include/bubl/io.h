4c4ac78463ef4590153833b6e2375e80af587ea2
Alessandro Rubini <rubini@gnudd.com>
2010-10-18 10:36:33 +0200

    serial: added input functions

--- include/bubl/io.h
+++ include/bubl/io.h
@@ -3,6 +3,9 @@
 
 extern void putc(int c);
 extern void puts(const char *s);
+extern int getc(void);
+extern int testc(void);
+extern char *gets(char *s, int len);
 extern void serial_setup(void);
 
 #endif /* __TSW_IO_H__ */
--- serial.c
+++ serial.c
@@ -19,13 +19,15 @@ enum ser_regs {
 	SER_PID,
 	SER_UNUSED_2C,
 
-
 	SER_PWR,
 	SER_MDR
 };
 
+/* Bits in line status register */
 #define TX_EMPTY	0x20
+#define RX_DR		0x01
 
+/* output functions */
 void putc(int c)
 {
 	volatile u32 *base = SERIAL_BASE;
@@ -46,6 +48,47 @@ void puts(const char *s)
 		putc(*(s++));
 }
 
+/* input functions */
+int testc(void)
+{
+	volatile u32 *base = SERIAL_BASE;
+
+	if (base[SER_LSR] & RX_DR)
+		return 1;
+	return 0;
+}
+
+int getc(void)
+{
+	volatile u32 *base = SERIAL_BASE;
+
+	if (!testc())
+		return -1;
+	return base[SER_DATA];
+}
+
+char *gets(char *s, int len)
+{
+	int i, c;
+	for (i = 0; i < len - 1; ) {
+		while (!testc())
+			/* wait for char */;
+		c = getc();
+		if (c == 0x03) { /* ctrl-C */
+			puts("<INTERRUPTED>\n");
+			break;
+		}
+		if (c == '\r') c = '\n';
+		s[i++] = c;
+		if (c == '\n')
+			break;
+	}
+	s[i] = '\0';
+	return s;
+}
+
+
+/* initial setup */
 void serial_setup(void)
 {
 	volatile u32 *base = SERIAL_BASE;
