ca70f80234c631dc52b493ba3f7e7ec3048262e3
Alessandro Rubini <rubini@gnudd.com>
2010-12-13 00:17:52 +0100

    u-boot-compat.h: more and better stuff

--- include/u-boot-compat.h
+++ include/u-boot-compat.h
@@ -2,7 +2,22 @@
 typedef u8 uchar;
 typedef u16 ushort;
 typedef u32 uint;
-typedef u32 ulong;
+#define ulong unsigned long /* This way (not typedef) to prevent warnings */
 typedef ulong lbaint_t;
 #define printf printk
 typedef struct {int unused;} bd_t;
+
+#define min(X, Y)                               \
+        ({ typeof (X) __x = (X), __y = (Y);     \
+                (__x < __y) ? __x : __y; })
+
+#define max(X, Y)                               \
+        ({ typeof (X) __x = (X), __y = (Y);     \
+                (__x > __y) ? __x : __y; })
+
+#define MIN(x, y)  min(x, y)
+#define MAX(x, y)  max(x, y)
+
+/* These only live inside if(0), so just prototype them */
+extern void *malloc(int);
+extern void free(void *);
