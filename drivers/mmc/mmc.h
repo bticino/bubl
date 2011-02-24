ecf6abe8c46864a989004fd155d03fe5a7ecb3d1
Raffaele Recalcati <raffaele.recalcati@bticino.it>
2011-02-24 10:37:11 +0100

    mmc: warning fix added prototype
    Signed-off-by: Raffaele Recalcati <raffaele.recalcati@bticino.it>

--- drivers/mmc/mmc.h
+++ drivers/mmc/mmc.h
@@ -287,6 +287,8 @@ struct mmc *find_mmc_device(int dev_num);
 int mmc_set_dev(int dev_num);
 void print_mmc_devices(char separator);
 int board_mmc_getcd(u8 *cd, struct mmc *mmc);
+int mmc_read_blocks(struct mmc *mmc, void *dst, ulong start, lbaint_t blkcnt);
+
 
 #ifdef CONFIG_GENERIC_MMC
 int atmel_mci_init(void *regs);
